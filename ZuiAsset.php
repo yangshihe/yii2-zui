8e851313dd3c3d124872b289e2937af7baa1cd28
chuanzhang/zui <yangshihe@qq.com>
2016-12-05 15:52:26 +0800

    完成样式配置

--- ZuiAsset.php
+++ ZuiAsset.php
@@ -11,64 +11,64 @@ use yii\web\AssetBundle;
 class ZuiAsset extends AssetBundle {
 
 	public $css = [
-        '//cdn.bootcss.com/zui/1.5.0/css/zui.min.css'
+		'//cdn.bootcss.com/zui/1.5.0/css/zui.min.css',
 
 	];
 
 	public $js = [
-        '//cdn.bootcss.com/zui/1.5.0/js/zui.min.js',
-        'js/public.js',
+		'//cdn.bootcss.com/zui/1.5.0/js/zui.min.js',
+		'js/public.js',
 	];
 
-    public $depends = [
-        'yii\web\YiiAsset',
-    ];
+	public $depends = [
+		'yii\web\YiiAsset',
+	];
 
-	public function init()
-    {
+	public function init() {
 
-        parent::init();
+		parent::init();
 
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
+		$this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
 
-        $this->cssOptions = ['name' => 'zui'];
+		$this->cssOptions = ['name' => 'zui'];
 
-        $this->setTheme();
-    }
+		$this->setTheme();
+	}
 
+	/*
+		    *   $this->setTheme();
+		    *  此主题不是Yii2 config['components']['view']['theme']['basePath']的视图路径,
+		    * 而是zui自身主题的切换
+	*/
+	private function setTheme() {
 
-    /*
-    *   $this->setTheme();
-    *  此主题不是Yii2 config['components']['view']['theme']['basePath']的视图路径,
-    * 而是zui自身主题的切换
-    */
-    private function setTheme()
-    {
+		$theme = 'default';
 
-        $theme = 'default';
+		$themeName = Yii::$app->request->get('theme');
 
-        $themeName = Yii::$app->request->get('theme');
+		$cookies = Yii::$app->request->cookies;
 
-        $cookies = Yii::$app->request->cookies;
+		if (!empty($themeName)) {
 
-        if (!empty($themeName)) {
+			$cssFile = realpath($this->sourcePath . '/css/zui-' . $themeName . '-theme.css');
 
-            $cssFile = realpath($this->sourcePath . '/css/zui-' . $themeName . '-theme.css');
+			if (file_exists($cssFile)) {
+				$theme = $themeName;
+			}
 
-            if(file_exists($cssFile)) $theme = $themeName;
+		} elseif ($cookies->has('theme')) {
 
-        } elseif ($cookies->has('theme')) {
+			$theme = $cookies['theme'];
 
-            $theme = $cookies['theme'];
+		}
+		$this->css[] = 'css/theme/zui-theme-' . $theme . '.css';
+		$this->css[] = 'css/app/zui-app-' . $theme . '.css';
+		// $this->css[] = 'css/public.css';
 
-        }
-        $this->css[] = 'css/zui-theme-' . $theme . '.css';
-        $this->css[] = 'css/public.css';
-
-        Yii::$app->response->cookies->add(new \yii\web\Cookie([
-            'name' => 'theme',
-            'value' => $theme,
-        ]));
-    }
+		Yii::$app->response->cookies->add(new \yii\web\Cookie([
+			'name' => 'theme',
+			'value' => $theme,
+		]));
+	}
 
 }
--- assets/js/public.js
+++ assets/js/public.js
@@ -1,15 +1,18 @@
-/*!
+﻿/*!
  * 全局js
  *
  */
+
 $(document).ready(function() {
     /*
-     *	动态加载 theme css
+     *  动态加载 theme css
      */
     var themeName = 'default',
         themeLinks = $('head').find('link[name="zui"]'),
-        url, linkPath, themeHandle,
-        themeReg = /zui-theme-([a-z])+\.css/i;
+        url, linkPath, themeHandle,themeAppHandle,
+        themeReg = /zui-theme-([a-z])+\.css/i,
+        themeRegApp = /zui-app-([a-z])+\.css/i;
+        var tlink;
     themeLinks.each(function(index) {
         url = $(this).attr('href');
         if (themeReg.exec(url)) {
@@ -18,15 +21,20 @@ $(document).ready(function() {
             themeName = themeReg.exec(url)[0].replace('zui-theme-', '').replace('.css', '');
             $('#selectTheme li[data-theme=' + themeName + ']').addClass('active');
         }
+        if (themeRegApp.exec(url)) {
+            tlink = themeRegApp.exec(url).input.substr(0, themeRegApp.exec(url).index);
+            themeAppHandle = $(this);
+        }
     });
 
     $(document).on('click', '#selectTheme li', function(event) {
         event.preventDefault();
         $(this).siblings('li').removeClass('active');
         $(this).addClass('active');
+        if ($(this).data('theme') == themeName) return;
         themeName = $(this).data('theme');
         themeHandle.after('<link rel="stylesheet" href="' + linkPath + 'zui-theme-' + themeName + '.css' + '">');
-
+        themeAppHandle.after('<link rel="stylesheet" href="' + tlink + 'zui-app-' + themeName + '.css' + '">');
         $.ajax({
                 url: '',
                 cache: false,
@@ -36,22 +44,47 @@ $(document).ready(function() {
             .done(function() {
                 setTimeout(function() {
                     var oldTheme = themeHandle;
+                    var oldThemeAppHandle = themeAppHandle;
                     themeHandle = oldTheme.next();
+                    themeAppHandle = oldThemeAppHandle.next();
                     oldTheme.remove();
+                    oldThemeAppHandle.remove();
                 }, 1000);
             });
     });
 
-    //表单防止多次提交
-    $('form').on('beforeSubmit', function (e) {
-        $(':submit').attr('disabled', true).addClass('disabled');
+    var showLoadingOption = {
+        showHeader: false,
+        remote: '',
+        waittime: 0,
+        position: 'center',
+        backdrop: 'static',
+        keyboard: false,
+        size: 'sm',
+        width: '0px',
+        id: 'loading',
+        loadingIcon: 'icon-spinner'
+    };
+    var showLoading = new $.zui.ModalTrigger(showLoadingOption);
+    //ajax 提交 后面要是不集成 可以使用 global: false,来决定
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            $('[type=submit]').attr('disabled', true).addClass('disabled');
+            showLoading.show();
+        },
+        complete: function() {
+            $('[type=submit]').attr('disabled', false).removeClass('disabled');
+            showLoading.close();
+        }
     });
-    //ajax 提交完成后解锁
-    $('body').on('ajaxComplete', 'form', function(){
-        $(':submit').attr('disabled', false).removeClass('disabled');
+    //普通表单防止多次提交
+    $('form').on('submit', function(e) {
+        $('[type=submit]').attr('disabled', true).addClass('disabled');
+        showLoading.show();
     });
 
-    (new $.zui.ModalTrigger({showHeader: false, custom:'<i class="icon icon-4x icon-spin icon-spinner"></i>'})).show();
+
+
 
     /**
      * 超级模态框数据回调
@@ -60,23 +93,23 @@ $(document).ready(function() {
      */
     /*
     设置触发
-	    A
-    	<button data-remote="url?group=modal_group1" data-toggle="modal">click</button>
+        A
+        <button data-remote="url?group=modal_group1" data-toggle="modal">click</button>
     设置接收
-    		data-name =指定要被回调的单元名称,
-    		data-text 设置此项表示回调的值写入到DOM的文字部分 不设置将写入 value部分,如input
-    	<input type="text" data-group="modal_group1" data-name="userid" value="">
-    	<span data-group="modal_group1" data-name="username" data-text="text"></span>
+            data-name =指定要被回调的单元名称,
+            data-text 设置此项表示回调的值写入到DOM的文字部分 不设置将写入 value部分,如input
+        <input type="text" data-group="modal_group1" data-name="userid" value="">
+        <span data-group="modal_group1" data-name="username" data-text="text"></span>
     远程模态框 body下的回调按钮
-    	<button data-group="<?php echo $_GET['group']?>" data-data='{"userid":1,"username":"小明"}' data-modal-group="callback">回调按钮</button>
-	接收的DOM 的最终结果
-    	<input type="text" data-group="modal_group1" data-name="userid" value="1">
-    	<span data-group="modal_group1" data-name="username" data-text="text">小明</span>
+        <button data-group="<?php echo $_GET['group']?>" data-data='{"userid":1,"username":"小明"}' data-modal-group="callback">回调按钮</button>
+    接收的DOM 的最终结果
+        <input type="text" data-group="modal_group1" data-name="userid" value="1">
+        <span data-group="modal_group1" data-name="username" data-text="text">小明</span>
      */
-    $(document).on('click', '[data-modal-group]', function () {
+    $(document).on('click', '[data-modal-group]', function() {
         var group = $(this).data('group'),
             data = $(this).data('data');
-        $('[data-group="' + group + '"]').each(function () {
+        $('[data-group="' + group + '"]').each(function() {
             var name = $(this).data('name');
             var text = $(this).data('text'); // text // val default val
             if (text === 'text') {
